a363aefd9633ff7467cadcc203d6f922a25c892f
luxinflore <ayagare@gmail.com>
2025-07-02 17:48:16 -0400

    Diff gif once again

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('.sparkle-cat').forEach(link => {
   link.addEventListener('mouseenter', () => {
     const cat = document.createElement('img');
-    cat.src = 'https://64.media.tumblr.com/e642b66f8548fa1485621bd82d3eacc9/df8c8427627025f7-72/s640x960/af7dc108c6fd722e684f767aa7ad6b9f72f9d9c9.gif';
+    cat.src = 'https://s1.ezgif.com/tmp/ezgif-120326da7e518e.gif';
     cat.classList.add('cat-jump');
 
     // Position relative to hovered element
